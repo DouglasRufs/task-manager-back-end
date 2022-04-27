924f35b40147f65e054bfaf8e02b9ff55b5842d6
Douglas Rufino <douglas.rufino.luz@hotmail.com>
2022-04-26 21:05:44 -0300

    feat: add route to get a task by id

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const mongoose = require("mongoose");
 const connectToDatabase = require("./src/database/mongoose.database");
 const TaskModel = require("./src/models/task.model");
 const req = require("express/lib/request");
+const res = require("express/lib/response");
 
 dotenv.config();
 const app = express();
@@ -19,6 +20,20 @@ app.get("/tasks", async (req, res) => {
     try {
         const tasks = await TaskModel.find({});
         res.status(200).send(tasks);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+app.get("/tasks/:id", async (req, res) => {
+    try {
+        const taskId = req.params.id;
+        const task = await TaskModel.findById(taskId );
+
+        if (!task) {
+            return res.status(404).send("Essa tarefa não foi encontrada.");
+        }
+        return res.status(200).send(task);
     } catch (error) {
         res.status(500).send(error.message);
     }
@@ -43,7 +58,7 @@ app.delete("/tasks/:id", async (req, res) => {
         const taskToDelete = await TaskModel.findById(taskId);
 
         if (!taskToDelete) {
-            return res.status(500).send("Essa tarefa não foi encontrada");
+            return res.status(404).send("Essa tarefa não foi encontrada");
         }
 
         const deletedTask = await TaskModel.findByIdAndDelete(taskId);
