48d31207e00eea9d3b5d230b96d6743e11b0cc66
Douglas Rufino <douglas.rufino.luz@hotmail.com>
2022-04-29 13:53:18 -0300

    feat:add cors

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // importação de biblioteca
-
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-const TaskRouter =require('./src/routes/task.routes')
+const TaskRouter = require("./src/routes/task.routes");
+const cors = require("cors");
 
-// importeçao de arquivo
+// importação de arquivo
 const connectToDatabase = require("./src/database/mongoose.database");
 const TaskModel = require("./src/models/task.model");
 const req = require("express/lib/request");
@@ -13,12 +13,13 @@ const res = require("express/lib/response");
 const { update } = require("./src/models/task.model");
 
 dotenv.config();
+
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 connectToDatabase();
 
-app.use('/tasks', TaskRouter);
-
+app.use("/tasks", TaskRouter);
 
 app.listen(8000, () => console.log("listening on port 8000!"));
